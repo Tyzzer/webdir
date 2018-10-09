5febc7a6a652181e9282c5963bec73f7e62cbd6b
quininer <quininer@live.com>
2018-10-09 14:52:06 +0800

    Remove old code

--- build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    #[cfg(any(target_os = "macos", target_os = "ios"))]
-    println!("cargo:rustc-cfg=apple");
-
-    #[cfg(any(target_os = "freebsd", target_os = "dragonfly"))]
-    println!("cargo:rustc-cfg=freebsdlike");
-}
